3a8661f143b6e202ac05ac2d7abeebedfa1e887f
Yaël TRAMIER <tramier.yael@gmail.com>
2023-10-23 14:29:45 +0200

    Added prompt for PIN

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import com.sun.javacard.apduio.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.print("Entrer le code PIN de la carte: ");
+        Scanner in = new Scanner(System.in);
+        String in_code = in.nextLine();
     }
 }
