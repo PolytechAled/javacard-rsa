c2ac1fe4a479e44e74830159bd61b3e668261886
Yaël TRAMIER <tramier.yael@gmail.com>
2023-10-23 15:12:00 +0200

    Added secure entry pin

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
 import com.sun.javacard.apduio.*;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        byte[] pin = new byte[4];
         System.out.print("Entrer le code PIN de la carte: ");
-        Scanner in = new Scanner(System.in);
-        String in_code = in.nextLine();
+        try{
+            System.in.readNBytes(pin, 0, pin.length);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        for (int i = 0; i < pin.length; i++){
+            if(pin[i] >= '0' && pin[i] <= '9')
+                pin[i] = (byte) (pin[i]-'0');
+            else{
+                System.out.println("Bad format");
+                System.exit(1);
+            }
+        }
     }
 }
